8a19aa9a449dd656646df40d7a37d153f0c93dfe
Omar Tarek <omarmoharam193@gmail.com>
2024-07-07 01:11:41 +0300

    Fix UCSRC value

--- 1-MCAL/7-UART/UART_Program.c
+++ 1-MCAL/7-UART/UART_Program.c
@@ -15,6 +15,7 @@
 void UART_voidInit(u16 Copy_u16BaudRate)
 {
     u16 Local_u8UBRRValue;
+    u8 Local_u8UCSRCValue = 0b10000000;
 
     /******************** Write the baud rate ********************/
     #if (U2X_MODE == DISABLE) && (USART_MODE == ASYNCHRONOUS)
@@ -36,7 +37,7 @@ void UART_voidInit(u16 Copy_u16BaudRate)
     /******************* Select Mode Asynchronous, Synchronous *******************/
     #if USART_MODE == ASYNCHRONOUS
 
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (0<<UART_u8_UCSRC_UMSEL_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UMSEL_BIT);
     
     /* Disable MPCM */
     CLR_BIT(UART_u8_UCSRA_REGISTER, UART_u8_UCSRA_MPCM_BIT);
@@ -49,7 +50,7 @@ void UART_voidInit(u16 Copy_u16BaudRate)
     
     #elif USART_MODE == SYNCHRONOUS
     
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UMSEL_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UMSEL_BIT);
 
     /* Disable U2X */
     CLR_BIT(UART_u8_UCSRA_REGISTER, UART_u8_UCSRA_U2X_BIT);
@@ -68,45 +69,54 @@ void UART_voidInit(u16 Copy_u16BaudRate)
     /******************* Choose Character Size *******************/
     #if CHARCTER_SIZE == FIVE_BITS
     CLR_BIT(UART_u8_UCSRB_REGISTER, UART_u8_UCSRB_UCSZ2_BIT);
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (0<<UART_u8_UCSRC_UCSZ1_BIT) | (0<<UART_u8_UCSRC_UCSZ0_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ1_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ0_BIT);
    
     #elif CHARCTER_SIZE == SIX_BITS
     CLR_BIT(UART_u8_UCSRB_REGISTER, UART_u8_UCSRB_UCSZ2_BIT);
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (0<<UART_u8_UCSRC_UCSZ1_BIT) | (1<<UART_u8_UCSRC_UCSZ0_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ1_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ0_BIT);
    
     #elif CHARCTER_SIZE == SEVEN_BITS
     CLR_BIT(UART_u8_UCSRB_REGISTER, UART_u8_UCSRB_UCSZ2_BIT);
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UCSZ1_BIT) | (0<<UART_u8_UCSRC_UCSZ0_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ1_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ0_BIT);
 
     #elif CHARCTER_SIZE == EIGHT_BITS
     CLR_BIT(UART_u8_UCSRB_REGISTER, UART_u8_UCSRB_UCSZ2_BIT);
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UCSZ1_BIT) | (1<<UART_u8_UCSRC_UCSZ0_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ1_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ0_BIT);
     
     #elif CHARCTER_SIZE == NINE_BITS
     SET_BIT(UART_u8_UCSRB_REGISTER, UART_u8_UCSRB_UCSZ2_BIT);
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UCSZ1_BIT) | (1<<UART_u8_UCSRC_UCSZ0_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ1_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UCSZ0_BIT);
     #endif
 
     /******************* Choose Parity bit *******************/
     #if PARITY_BIT == ENABLE_EVEN
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UPM1_BIT) | (0<<UART_u8_UCSRC_UPM0_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM1_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM0_BIT);
 
     #elif PARITY_BIT == ENABLE_ODD
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_UPM1_BIT) | (1<<UART_u8_UCSRC_UPM0_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM1_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM0_BIT);
 
     #elif PARITY_BIT == DISABLE
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (0<<UART_u8_UCSRC_UPM1_BIT) | (0<<UART_u8_UCSRC_UPM0_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM1_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_UPM0_BIT);
     #endif
     
 
     /******************* Choose Stop Bit *******************/
     #if STOP_BIT == ONE_BIT
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (0<<UART_u8_UCSRC_USBS_BIT);
+    CLR_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_USBS_BIT);
 
     #elif STOP_BIT == TWO_BITS
-    UART_u8_UCSRC_REGISTER |= (1<<UART_u8_UCSRC_URSEL_BIT) | (1<<UART_u8_UCSRC_USBS_BIT);
+    SET_BIT(Local_u8UCSRCValue, UART_u8_UCSRC_USBS_BIT);
     #endif
 
+    UART_u8_UCSRC_REGISTER = Local_u8UCSRCValue;
 }
 
 void UART_voidSendByte(u8 Copy_u8SendData)
